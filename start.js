78ec9d009cf93db25255fad42ee45d6f121ea581
adebai <theadebai@gmail.com>
2022-08-20 04:36:30 +0100

    add some comments to start.js

--- start.js
+++ start.js
@@ -6,8 +6,8 @@ let cmd = new Command("start");
 cmd.command("generate-json <source> <destination> <extension>")
     .description("Generates a compact JSON version of your input javascript file."+ 
     "The JSON file can be distributed easily since it is smaller in size." + 
-    " Supported extensions: .json, .hex and .bin. Note that .bin might not generate an actual binary data. Check https://developer.mozilla.org/en-US/docs/Web/API/DOMString/Binary" +
-    "cfor more info. Support for more extensions will be added later. Also note that direct use of any of these compact file generated is yet to be implemented")
+    " Supported extensions: .json, .hex and .bin. Note that .bin might not generate an actual binary data. Check https://developer.mozilla.org/en-US/docs/Web/API/DOMString/Binary " +
+    "for more info. Support for more extensions will be added later. Also note that direct use of any of these compact file generated is yet to be implemented")
     .action(function (source, destination, extension) {
         let instance = new processEngine(GENERATESCHEMA, source, destination + "." + extension);
         // Got further things to do?
