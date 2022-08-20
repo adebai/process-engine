d8bba08c4ecb54c2e1fbe077d2e6cec1dd1fe10f
adebai <theadebai@gmail.com>
2022-08-20 04:17:35 +0100

    edit readme

--- start.js
+++ start.js
@@ -4,10 +4,10 @@ const { processEngine } = require('./processEngine');
 let cmd = new Command("start");
 
 cmd.command("generate-json <source> [destination] <extension>")
-    .description("Generates a compact JSON version of your input javascript \nfile."+ 
+    .description("Generates a compact JSON version of your input javascript file."+ 
     "The JSON file can be distributed easily since it is smaller in size" + 
-    "\n Supported extension: .json, .hex and .bin. \nNote that .bin might not generate an actual binary data.\n Check https://developer.mozilla.org/en-US/docs/Web/API/DOMString/Binary" +
-    "for more info. \nSupport for more extensions will be added later.")
+    " Supported extension: .json, .hex and .bin. Note that .bin might not generate an actual binary data. Check https://developer.mozilla.org/en-US/docs/Web/API/DOMString/Binary" +
+    "for more info. Support for more extensions will be added later.")
     .action(function (source, destination) {
         let instance = new processEngine(GENERATESCHEMA, source, destination);
 
